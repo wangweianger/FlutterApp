d806742af44a03d58a1e45fcff688af5f8cb2907
wangweianger <752636052@qq.com>
2018-12-14 15:33:11 +0800

    add demo

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -7,12 +7,20 @@ class __HomePageState extends State<HomePage>{
 	@override
 	Widget build(BuildContext context) {
 		return new MaterialApp(
+            theme: new ThemeData(
+                primarySwatch: Colors.red,
+            ),
 			home:new Scaffold(
 				appBar: AppBar(
 					title: Text('Home Page'),
 				),
 				body:new Center(
-                    child: new Text('Home page! Come on.'),
+                    child: new Text(
+                        'Home page! Come on.',
+                        style:TextStyle(
+                            fontSize: 30.0
+                        )
+                    ),
                 ),
 			),
 		);
